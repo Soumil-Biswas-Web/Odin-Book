aae2cf5a79d4205c61ebaa513786b9fd0018c30d
Soumil Biswas <soumil.biswas19@gmail.com>
2025-05-01 20:32:00 +0530

    Added cleaners to loader to avoid memory leaks

--- frontend/src/Content/Components/Loader.jsx
+++ frontend/src/Content/Components/Loader.jsx
@@ -5,7 +5,7 @@ function Loader() {
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        axios.interceptors.request.use(
+        const reqInterceptor = axios.interceptors.request.use(
             (config) => {
                 setLoading(true);
                 return config;
@@ -15,8 +15,8 @@ function Loader() {
                 return Promise.reject(error);
             }
         );
-
-        axios.interceptors.response.use(
+    
+        const resInterceptor = axios.interceptors.response.use(
             (response) => {
                 setLoading(false);
                 return response;
@@ -26,6 +26,12 @@ function Loader() {
                 return Promise.reject(error);
             }
         );
+    
+        // Cleanup function
+        return () => {
+            axios.interceptors.request.eject(reqInterceptor);
+            axios.interceptors.response.eject(resInterceptor);
+        };
     }, []);
 
     if (loading) {
@@ -34,7 +40,7 @@ function Loader() {
                 <div className="h-[50px] w-[50px] md:h-[80px] md:w-[80px] rounded-full border-b-2 border-b-contrast-color animate-spin"></div>
             </div>
         );
-    }
+    } else return null;
 }
 
 export default Loader
